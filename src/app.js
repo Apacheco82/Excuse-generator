b6b8908d75bdfd4c6a4cd7bc636291a7abbe02d1
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2023-01-03 14:14:22 +0100

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+window.onload = function() {
+    let who = [
+      "Mi gato",
+      "Su abuela",
+      "El loro del vecino",
+      "Pikachu",
+      "El repartidor de Amazon",
+      "Tu prima la del pueblo",
+      "El profesor"
+    ];
+    let what = ["se lo comió", "lo rompió", "explotó", "escupió"];
+    let when = [
+      "después de clase",
+      "justo a tiempo",
+      "cuando había terminado",
+      "mientras comía",
+      "cuando me estaba duchando",
+      "cuando iba a finalizar"
+    ];
+    let primer = Math.floor(Math.random() * who.length);
+    let segundo = Math.floor(Math.random() * what.length);
+    let tercer = Math.floor(Math.random() * when.length);
+  
+    let output = who[primer] + " " + what[segundo] + " " + when[tercer];
+  
+    document.getElementById("excuse").innerHTML = output;
+  };
